257e5c52d73340a7cda0015c05a73c17886d7ed8
Jahangir Khan <jahangir.khan@emumba.com>
2020-06-25 15:49:06 +0500

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from "react";
+import { MenuOutlined } from "@ant-design/icons";
 
-import "./navbar.scss"
+import "./navbar.scss";
 
 function Navbar() {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
+  return (
+    <nav className="navigation-bar">
+      <input type="checkbox" id="check" />
+      <label for="check" className="check-button">
+        <MenuOutlined />
+      </label>
+      <figure className="brand-logo">
+        <picture className="logo">
+          <img src="./images/logo.png" className="logo-img" />
+        </picture>
+        <figcaption>
+          <h2 className="logo-title">MARKUP TEAM</h2>
+        </figcaption>
+      </figure>
+      <ul className="nav-ul">
+        <li className="nav-li">
+          <a href="#" className="nav-link">
+            Hierachy
+          </a>
+        </li>
+        <li className="nav-li">
+          <a href="#" className="nav-link">
+            Knowledge Share
+          </a>
+        </li>
+        <li className="nav-li">
+          <a href="#" className="nav-link">
+            Learning Path
+          </a>
+        </li>
+        <li className="nav-li">
+          <a href="#" className="nav-link">
+            Mentors and Mentees
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
