f31a8b48ea74783392c13b404a7960883dfb00a4
Jahangir Khan <jahangir.khan@emumba.com>
2020-07-01 13:11:41 +0500

    Updates layout and added mixin

--- src/components/CutomDropDown/CustomDropDown.js
+++ src/components/CutomDropDown/CustomDropDown.js
@@ -6,13 +6,18 @@ import "./customDropDown.scss";
 const { Panel } = Collapse;
 
 function CustomDropDown(props) {
-  const { learningPathData } = props
+  const { learningPathData } = props;
   return (
     <Collapse className="drop-down">
       {learningPathData.map((data, index) => (
         <Panel header={data.title} key={index}>
           {data.content.map((webLinks, index) => (
-            <a key={index} href={webLinks.url} className="web-links" target="_blank">
+            <a
+              key={index}
+              href={webLinks.url}
+              className="web-links"
+              target="_blank"
+            >
               {webLinks.link}
             </a>
           ))}
